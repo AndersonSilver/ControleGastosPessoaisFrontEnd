bde3573716bd39702b32b47536f809dceb4b3a4b
AndersonSilver <andersonsilver18@gmail.com>
2023-04-17 05:23:31 -0300

    Aty Search Dados User

--- src/public/js/SearchDadosUser.js
+++ src/public/js/SearchDadosUser.js
@@ -1,5 +1,9 @@
-async function dadosLogin(){
-    const email = document.getElementById("SerChamado").value;
 
-    console.log(email);
+async function dadosLogin() {
+
+    const response = await fetch(`http://localhost:3000/searchDadosPessoais?email=teste@teste.com`);
+    const result = await response.json();
+    const dados = result;
+    console.log(dados);
 }
+
