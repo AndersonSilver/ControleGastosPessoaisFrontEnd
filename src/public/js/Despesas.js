78867e608e5856f7730bc9c705fbe45e6b4933f4
AndersonSilver <andersonsilver18@gmail.com>
2023-04-25 21:28:07 -0300

    att

--- src/public/js/Despesas.js
+++ src/public/js/Despesas.js
@@ -40,6 +40,39 @@ async function searchAllUsersDespesas(){
     const ContaCell = row.insertCell();
     ContaCell.innerText = user.conta;
     
+    const acoesCell = row.insertCell();
+
+    // acoesCell.style.width = "150px";
+    const botaoEditarAcoes = document.createElement('button'); // Cria o elemento de botão
+    const botaoDeletarAcoes = document.createElement('button');  
+
+    botaoEditarAcoes.innerText = 'Editar'; // Define o texto do botão
+    botaoEditarAcoes.classList.add('btnReceita'); // Adiciona classes ao botão, se necessário
+    botaoEditarAcoes.style.display = 'inline-block';
+    // Adicione um event listener ao botão, se desejar
+    botaoEditarAcoes.addEventListener('click', () => {
+      
+      // Lógica da ação do botão aqui
+    console.log('Botão clicado!', user); // Exemplo de ação do botão, você pode substituir por sua lógica
+    });
+
+    acoesCell.appendChild(botaoEditarAcoes); // Adiciona o botão à célula da coluna "Ações"
+
+
+    botaoDeletarAcoes.innerText = 'Deletar'; // Define o texto do botão
+    botaoDeletarAcoes.classList.add('btnReceita'); // Adiciona classes ao botão, se necessário
+    botaoDeletarAcoes.style.display = 'inline-block';
+    // Adicione um event listener ao botão, se desejar
+    botaoDeletarAcoes.addEventListener('click', () => {
+      
+      // Lógica da ação do botão aqui
+    console.log('Botão clicado!', user); // Exemplo de ação do botão, você pode substituir por sua lógica
+    });
+
+    acoesCell.appendChild(botaoDeletarAcoes); // Adiciona o botão à célula da coluna "Ações"
+
+    
+
     });
 
 }
--- src/public/js/Receitas.js
+++ src/public/js/Receitas.js
@@ -40,13 +40,17 @@ async function searchAllUsers(){
     const ContaCell = row.insertCell();
     ContaCell.innerText = user.conta;
 
+    
+
     const acoesCell = row.insertCell();
+
+    // acoesCell.style.width = "150px";
     const botaoEditarAcoes = document.createElement('button'); // Cria o elemento de botão
-    const botaoDeletarAcoes = document.createElement('button');   
+    const botaoDeletarAcoes = document.createElement('button');  
 
     botaoEditarAcoes.innerText = 'Editar'; // Define o texto do botão
-    botaoEditarAcoes.classList.add('btn'); // Adiciona classes ao botão, se necessário
-    
+    botaoEditarAcoes.classList.add('btnReceita'); // Adiciona classes ao botão, se necessário
+    botaoEditarAcoes.style.display = 'inline-block';
     // Adicione um event listener ao botão, se desejar
     botaoEditarAcoes.addEventListener('click', () => {
       
@@ -56,9 +60,10 @@ async function searchAllUsers(){
 
     acoesCell.appendChild(botaoEditarAcoes); // Adiciona o botão à célula da coluna "Ações"
 
+
     botaoDeletarAcoes.innerText = 'Deletar'; // Define o texto do botão
-    botaoDeletarAcoes.classList.add('btn'); // Adiciona classes ao botão, se necessário
-    
+    botaoDeletarAcoes.classList.add('btnReceita'); // Adiciona classes ao botão, se necessário
+    botaoDeletarAcoes.style.display = 'inline-block';
     // Adicione um event listener ao botão, se desejar
     botaoDeletarAcoes.addEventListener('click', () => {
       
